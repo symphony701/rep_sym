f982f1fc34aabc963bc32239e540f492fe009413
Symphony <rnsebastian701@gmail.com>
2020-02-18 20:43:29 -0500

    add flamingo

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log("Hola mundo!");
+var reproductor= document.querySelector('#reproductor')
+const flamingo= document.querySelector("#flamingo");
+
+const referencia_1= document.querySelector('#reference');
+const referencia_2= document.querySelector('#referencia_mini');
+
+flamingo.addEventListener('click',()=>{
+    referencia_1.setAttribute("src", "./portadas/flamingo.jpeg");
+    referencia_2.setAttribute("src", "./portadas/flamingo.jpeg");
+    reproductor.setAttribute("src" , "./musicauu/flamingo.mp3");
+} )
