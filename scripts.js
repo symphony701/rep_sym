117c92367f686e3f0a8352680ced7963f3df912b
Symphony <rnsebastian701@gmail.com>
2020-02-18 21:54:30 -0500

    new flamingo

--- scripts.js
+++ scripts.js
@@ -1,11 +1,31 @@
-var reproductor= document.querySelector('#reproductor')
-const flamingo= document.querySelector("#flamingo");
 
+var flamingo ={
+    identify : document.querySelector("#flamingo"),
+    title : document.querySelector('#T_flamingo').innerHTML,
+    artist : document.querySelector('#A_flamingo').innerHTML
+};
+
+
+
+var reproductor= document.querySelector('#reproductor');
+var t_rep=document.querySelector('#A_reproductor');
+var t_art=document.querySelector('#T_reproductor');
 const referencia_1= document.querySelector('#reference');
 const referencia_2= document.querySelector('#referencia_mini');
 
-flamingo.addEventListener('click',()=>{
+
+
+flamingo.identify.addEventListener('click',()=>{
     referencia_1.setAttribute("src", "./portadas/flamingo.jpeg");
     referencia_2.setAttribute("src", "./portadas/flamingo.jpeg");
     reproductor.setAttribute("src" , "./musicauu/flamingo.mp3");
+    t_rep.innerHTML=flamingo.title;
+    t_art.innerHTML=flamingo.artist;
+    
+    
 } )
+
+
+
+
+
