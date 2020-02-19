3c71973222f50bf78da37b0960286407d4f5383a
Symphony <rnsebastian701@gmail.com>
2020-02-18 22:26:34 -0500

    moment upload

--- scripts.js
+++ scripts.js
@@ -8,8 +8,8 @@ var flamingo ={
 
 
 var reproductor= document.querySelector('#reproductor');
-var t_rep=document.querySelector('#A_reproductor');
-var t_art=document.querySelector('#T_reproductor');
+var a_rep=document.querySelector('#A_reproductor');
+var t_rep=document.querySelector('#T_reproductor');
 const referencia_1= document.querySelector('#reference');
 const referencia_2= document.querySelector('#referencia_mini');
 
@@ -20,7 +20,7 @@ flamingo.identify.addEventListener('click',()=>{
     referencia_2.setAttribute("src", "./portadas/flamingo.jpeg");
     reproductor.setAttribute("src" , "./musicauu/flamingo.mp3");
     t_rep.innerHTML=flamingo.title;
-    t_art.innerHTML=flamingo.artist;
+    a_rep.innerHTML=flamingo.artist;
     
     
 } )
